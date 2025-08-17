3a5f3f210717613bd85f72a253d38cc0564b9b51
Michael-Eremin <ereminmmichail23@gmail.com>
2025-08-17 13:04:17 +0300

    init project

---
+++ src/main/java/com/example/opt_llm/OptLlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.opt_llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptLlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opt_llm/OptLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opt_llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
