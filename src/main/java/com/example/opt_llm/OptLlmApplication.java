80d272f36e80da839fe380db11db68d8f841e3aa
Michael-Eremin <ereminmmichail23@gmail.com>
2025-08-19 08:10:26 +0300

    add and fix settings

--- src/main/java/com/example/opt_llm/OptLlmApplication.java
+++ src/main/java/com/example/opt_llm/OptLlmApplication.java
@@ -9,6 +9,7 @@ import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,15 +43,41 @@ public class OptLlmApplication {
 	public ChatClient chatClient(ChatClient.Builder builder) {
 		return builder.defaultAdvisors(
 				getHistoryAdvisor(),
+						SimpleLoggerAdvisor.builder().build(),
+						getRagAdviser(),
 						SimpleLoggerAdvisor.builder().build()
-//				, getRagAdviser()
+				)
+				.defaultOptions(
+						OllamaOptions.builder()
+//								Лучше 0.2 - 0.3 (чтобы сильно не изобретала)
+								.temperature(0.3)
+//								Выбираем с вероятностью соответствия токена
+//								70% - в основном зайдет готовый токен около 70%
+//								и мало мусора, чтобы добить нехватку.
+//								Если взять больше (90%), то попадут
+//								наиболее подходящие (около 70%) но и много мусора
+//								добьется на оставшиеся 20%
+								.topP(0.7)
+//								количество подходящих токенов
+//								сколько взять токенов
+//								(должна быть вариативность, но небольшая)
+								.topK(20)
+//								Штраф за повтор - не большой, но должен быть
+								.repeatPenalty(1.1)
+								.build()
 				)
 				.build();
 	}
 
 	private Advisor getRagAdviser() {
 		return QuestionAnswerAdvisor.builder(vectorStore).promptTemplate(MY_PROMPT_TEMPLATE).searchRequest(
-				SearchRequest.builder().topK(4).build()
+				SearchRequest.builder()
+//						Сколько взять чанков
+//						если взять 40, то ждать ответ минут 10
+						.topK(4)
+//						как topP в chatClient для токенов, а этот для чанков
+						.similarityThreshold(0.65)
+						.build()
 		).build();
 	}
 
